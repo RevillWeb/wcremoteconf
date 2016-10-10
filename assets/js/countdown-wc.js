3429bfc646cf67502c9a3f2830cd15e8f52d281c
RevillWeb <leon@revillweb.com>
2016-10-10 18:36:14 +0100

    Fixed console error, updated both slide in and count down components.

--- assets/js/countdown-wc.js
+++ assets/js/countdown-wc.js
@@ -24,21 +24,16 @@ var CountdownWc = function (_HTMLElement) {
     /**
      * Construct the timer element with some initial markup and styling
      */
-    function CountdownWc() {
+    function CountdownWc(self) {
+        var _this, _ret;
+
         _classCallCheck(this, CountdownWc);
 
-        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(CountdownWc).call(this));
-
-        _this.attachShadow({ mode: 'open' });
-        _this.shadowRoot.innerHTML = "\n            <style>\n                @import url(https://fonts.googleapis.com/css?family=Oswald:400,300,700);\n                .countdown-timer-container {\n                    font-family: 'Oswald', sans-serif;\n                }\n                .countdown-timer-container .section {\n                    width: 120px;\n                    height: 150px;\n                    float: left;\n                    margin-right: 10px;\n                    position: relative;\n                }\n                .countdown-timer-container .section .count-container {\n                    height: 120px; \n                }\n                .countdown-timer-container .section .count-label {\n                    height: 30px;\n                    line-height: 30px;\n                    text-align: center;\n                }\n            </style>\n            <div class=\"countdown-timer-container\">\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"days\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">DAYS</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"hours\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">HOURS</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"minutes\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">MINUTES</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"seconds\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">SECONDS</div>\n                </div>\n            </div>\n            \n        ";
-
-        _this.$days = _this.shadowRoot.querySelector("#days");
-        _this.$hours = _this.shadowRoot.querySelector("#hours");
-        _this.$minutes = _this.shadowRoot.querySelector("#minutes");
-        _this.$seconds = _this.shadowRoot.querySelector("#seconds");
-
-        _this._interval = null;
-        return _this;
+        self = (_this = _possibleConstructorReturn(this, (CountdownWc.__proto__ || Object.getPrototypeOf(CountdownWc)).call(this, self)), _this);
+        self.attachShadow({ mode: 'open' });
+        self.shadowRoot.innerHTML = "\n            <style>\n                @import url(https://fonts.googleapis.com/css?family=Oswald:400,300,700);\n                .countdown-timer-container {\n                    font-family: 'Oswald', sans-serif;\n                }\n                .countdown-timer-container .section {\n                    width: 120px;\n                    height: 150px;\n                    float: left;\n                    margin-right: 10px;\n                    position: relative;\n                }\n                .countdown-timer-container .section .count-container {\n                    height: 120px; \n                }\n                .countdown-timer-container .section .count-label {\n                    height: 30px;\n                    line-height: 30px;\n                    text-align: center;\n                }\n            </style>\n            <div class=\"countdown-timer-container\">\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"days\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">DAYS</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"hours\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">HOURS</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"minutes\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">MINUTES</div>\n                </div>\n                <div class=\"section\">\n                    <div class=\"count-container\">\n                        <countdown-wc-number id=\"seconds\"></countdown-wc-number>\n                    </div>\n                    <div class=\"count-label\">SECONDS</div>\n                </div>\n            </div>\n            \n        ";
+        self._interval = null;
+        return _ret = self, _possibleConstructorReturn(_this, _ret);
     }
 
     /**
@@ -89,6 +84,11 @@ var CountdownWc = function (_HTMLElement) {
         value: function connectedCallback() {
             var _this2 = this;
 
+            this.$days = this.shadowRoot.querySelector("#days");
+            this.$hours = this.shadowRoot.querySelector("#hours");
+            this.$minutes = this.shadowRoot.querySelector("#minutes");
+            this.$seconds = this.shadowRoot.querySelector("#seconds");
+            this.render();
             this._interval = setInterval(function () {
                 _this2.render();
             }, 1000);
@@ -121,9 +121,8 @@ var CountdownWc = function (_HTMLElement) {
          */
         value: function attributeChangedCallback(name, oldValue, newValue) {
             //"name" will only ever be the date attribute because of "observedAttributes"
-            if (newValue !== oldValue) {
+            if (newValue !== null && newValue !== oldValue) {
                 this.parseDateString(newValue);
-                this.render();
             }
         }
     }], [{
@@ -153,31 +152,38 @@ var CountdownWcNumber = function (_HTMLElement2) {
     /**
      * Construct the number eleemnt with some initial HTML markup and styling
      */
-    function CountdownWcNumber() {
+    function CountdownWcNumber(self) {
+        var _this3, _ret2;
+
         _classCallCheck(this, CountdownWcNumber);
 
-        var _this3 = _possibleConstructorReturn(this, Object.getPrototypeOf(CountdownWcNumber).call(this));
-
-        _this3.current = null;
-        _this3.next = null;
-        _this3.innerHTML = "\n            <style>\n                .count {        \n                  height: 120px;\n                  line-height: 120px;\n                  -moz-perspective: 320px;\n                  -webkit-perspective: 320px;\n                  perspective: 320px;\n                  position: absolute;\n                  text-align: center;                 \n                  -moz-transform: translateZ(0);\n                  -webkit-transform: translateZ(0);\n                  transform: translateZ(0);\n                  width: 120px;\n                }\n                .count span {\n                  background: #202020;\n                  color: #f8f8f8;\n                  display: block;\n                  font-size: 70px;\n                  left: 0;\n                  position: absolute;\n                  top: 0;                 \n                  -moz-transform-origin: 0 60px 0;\n                  -webkit-transform-origin: 0 60px 0;\n                  transform-origin: 0 60px 0;\n                  width: 100%;\n                }\n                .count.size4 span {\n                    font-size: 55px; \n                }\n                .count.size5 span {\n                    font-size: 45px; \n                }\n                .count.size6 span {\n                    font-size: 35px; \n                }\n                .count span:before {\n                  border-bottom: 2px solid #000;\n                  content: \"\";\n                  left: 0;\n                  position: absolute;\n                  width: 100%;\n                }\n                .count span:after {               \n                  content: \"\";\n                  height: 100%;\n                  left: 0;\n                  position: absolute;\n                  top: 0;\n                  width: 100%;\n                }\n                .count .top {                                   \n                  height: 50%;\n                  overflow: hidden;\n                }\n                .count .top:before {\n                  bottom: 0;\n                }               \n                .count .bottom {                 \n                  height: 100%;\n                }\n                .count .bottom:before {\n                  top: 50%;\n                }                \n                .count .top {\n                  height: 50%;\n                }\n                .count .top.current {\n                  -moz-transform-style: flat;\n                  -webkit-transform-style: flat;\n                  transform-style: flat;\n                  z-index: 3;\n                }\n                .count .top.next {\n                  -moz-transform: rotate3d(1, 0, 0, -90deg);\n                  -ms-transform: rotate3d(1, 0, 0, -90deg);\n                  -webkit-transform: rotate3d(1, 0, 0, -90deg);\n                  transform: rotate3d(1, 0, 0, -90deg);\n                  z-index: 4;\n                }\n                .count .bottom.current {\n                  z-index: 2;\n                }\n                .count .bottom.next {\n                  z-index: 1;\n                }\n                .count.changing .bottom.current {\n                  -moz-transform: rotate3d(1, 0, 0, 90deg);\n                  -ms-transform: rotate3d(1, 0, 0, 90deg);\n                  -webkit-transform: rotate3d(1, 0, 0, 90deg);\n                  transform: rotate3d(1, 0, 0, 90deg);\n                  -moz-transition: -moz-transform 0.35s ease-in;\n                  -o-transition: -o-transform 0.35s ease-in;\n                  -webkit-transition: -webkit-transform 0.35s ease-in;\n                  transition: transform 0.35s ease-in;\n                }\n                .count.changing .top.next, .count.changed .top.next {\n                  -moz-transition: -moz-transform 0.35s ease-out 0.35s;\n                  -o-transition: -o-transform 0.35s ease-out 0.35s;\n                  -webkit-transition: -webkit-transform 0.35s ease-out;\n                  -webkit-transition-delay: 0.35s;\n                  transition: transform 0.35s ease-out 0.35s;\n                  -moz-transform: none;\n                  -ms-transform: none;\n                  -webkit-transform: none;\n                  transform: none;\n                }\n                .count.changed .top.current,\n                .count.changed .bottom.current {\n                  display: none;\n                }\n            </style>\n            <div class=\"count\">           \n                <span class=\"current top\"></span>\n                <span class=\"next top\"></span>\n                <span class=\"current bottom\"></span>\n                <span class=\"next bottom\"></span>\n            </div>\n        ";
-        _this3.$count = _this3.querySelector(".count");
-        _this3.querySelector(".count .top.next").addEventListener("transitionend", function () {
-            //Clean up after the animation has been completed
-            _this3.$count.classList.add("changed");
-            _this3.$count.classList.remove("changing");
-            _this3.current = _this3.next;
-        });
-        return _this3;
+        self = (_this3 = _possibleConstructorReturn(this, (CountdownWcNumber.__proto__ || Object.getPrototypeOf(CountdownWcNumber)).call(this, self)), _this3);
+        self.current = null;
+        self.next = null;
+        self.innerHTML = "\n            <style>\n                .count {        \n                  height: 120px;\n                  line-height: 120px;\n                  -moz-perspective: 320px;\n                  -webkit-perspective: 320px;\n                  perspective: 320px;\n                  position: absolute;\n                  text-align: center;                 \n                  -moz-transform: translateZ(0);\n                  -webkit-transform: translateZ(0);\n                  transform: translateZ(0);\n                  width: 120px;\n                }\n                .count span {\n                  background: #202020;\n                  color: #f8f8f8;\n                  display: block;\n                  font-size: 70px;\n                  left: 0;\n                  position: absolute;\n                  top: 0;                 \n                  -moz-transform-origin: 0 60px 0;\n                  -webkit-transform-origin: 0 60px 0;\n                  transform-origin: 0 60px 0;\n                  width: 100%;\n                }\n                .count.size4 span {\n                    font-size: 55px; \n                }\n                .count.size5 span {\n                    font-size: 45px; \n                }\n                .count.size6 span {\n                    font-size: 35px; \n                }\n                .count span:before {\n                  border-bottom: 2px solid #000;\n                  content: \"\";\n                  left: 0;\n                  position: absolute;\n                  width: 100%;\n                }\n                .count span:after {               \n                  content: \"\";\n                  height: 100%;\n                  left: 0;\n                  position: absolute;\n                  top: 0;\n                  width: 100%;\n                }\n                .count .top {                                   \n                  height: 50%;\n                  overflow: hidden;\n                }\n                .count .top:before {\n                  bottom: 0;\n                }               \n                .count .bottom {                 \n                  height: 100%;\n                }\n                .count .bottom:before {\n                  top: 50%;\n                }                \n                .count .top {\n                  height: 50%;\n                }\n                .count .top.current {\n                  -moz-transform-style: flat;\n                  -webkit-transform-style: flat;\n                  transform-style: flat;\n                  z-index: 3;\n                }\n                .count .top.next {\n                  -moz-transform: rotate3d(1, 0, 0, -90deg);\n                  -ms-transform: rotate3d(1, 0, 0, -90deg);\n                  -webkit-transform: rotate3d(1, 0, 0, -90deg);\n                  transform: rotate3d(1, 0, 0, -90deg);\n                  z-index: 4;\n                }\n                .count .bottom.current {\n                  z-index: 2;\n                }\n                .count .bottom.next {\n                  z-index: 1;\n                }\n                .count.changing .bottom.current {\n                  -moz-transform: rotate3d(1, 0, 0, 90deg);\n                  -ms-transform: rotate3d(1, 0, 0, 90deg);\n                  -webkit-transform: rotate3d(1, 0, 0, 90deg);\n                  transform: rotate3d(1, 0, 0, 90deg);\n                  -moz-transition: -moz-transform 0.35s ease-in;\n                  -o-transition: -o-transform 0.35s ease-in;\n                  -webkit-transition: -webkit-transform 0.35s ease-in;\n                  transition: transform 0.35s ease-in;\n                }\n                .count.changing .top.next, .count.changed .top.next {\n                  -moz-transition: -moz-transform 0.35s ease-out 0.35s;\n                  -o-transition: -o-transform 0.35s ease-out 0.35s;\n                  -webkit-transition: -webkit-transform 0.35s ease-out;\n                  -webkit-transition-delay: 0.35s;\n                  transition: transform 0.35s ease-out 0.35s;\n                  -moz-transform: none;\n                  -ms-transform: none;\n                  -webkit-transform: none;\n                  transform: none;\n                }\n                .count.changed .top.current,\n                .count.changed .bottom.current {\n                  display: none;\n                }\n            </style>\n            <div class=\"count\">           \n                <span class=\"current top\"></span>\n                <span class=\"next top\"></span>\n                <span class=\"current bottom\"></span>\n                <span class=\"next bottom\"></span>\n            </div>\n        ";
+        return _ret2 = self, _possibleConstructorReturn(_this3, _ret2);
     }
 
-    /**
-     * Only observe the value attribute
-     * @returns {string[]}
-     */
-
-
     _createClass(CountdownWcNumber, [{
+        key: "connectedCallback",
+        value: function connectedCallback() {
+            var _this4 = this;
+
+            this.$count = this.querySelector(".count");
+            this.querySelector(".count .top.next").addEventListener("transitionend", function () {
+                //Clean up after the animation has been completed
+                _this4.$count.classList.add("changed");
+                _this4.$count.classList.remove("changing");
+                _this4.current = _this4.next;
+            });
+        }
+
+        /**
+         * Only observe the value attribute
+         * @returns {string[]}
+         */
+
+    }, {
         key: "setSize",
 
 
@@ -199,19 +205,19 @@ var CountdownWcNumber = function (_HTMLElement2) {
     }, {
         key: "render",
         value: function render() {
-            var _this4 = this;
+            var _this5 = this;
 
             if (this.current !== null) {
                 var $currents = this.querySelectorAll(".current");
                 [].forEach.call($currents, function (el) {
-                    el.innerText = _this4.current;
+                    el.innerText = _this5.current;
                 });
             }
             if (this.next !== null) {
                 this.setSize(this.next);
                 var $nexts = this.querySelectorAll(".next");
                 [].forEach.call($nexts, function (el) {
-                    el.innerText = _this4.next;
+                    el.innerText = _this5.next;
                 });
             }
         }
@@ -226,7 +232,7 @@ var CountdownWcNumber = function (_HTMLElement2) {
     }, {
         key: "attributeChangedCallback",
         value: function attributeChangedCallback(name, oldValue, newValue) {
-            var _this5 = this;
+            var _this6 = this;
 
             if (oldValue === null) {
                 var current = parseInt(newValue);
@@ -248,7 +254,7 @@ var CountdownWcNumber = function (_HTMLElement2) {
                 this.$count.classList.remove("changed");
                 this.$count.classList.remove("changing");
                 setTimeout(function () {
-                    _this5.$count.classList.add("changing");
+                    _this6.$count.classList.add("changing");
                 }, 20);
             }
         }
