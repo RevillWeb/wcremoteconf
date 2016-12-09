2e3a23f87fcba46e313c9155178a2c02201d4538
RevillWeb <leon@revillweb.com>
2016-12-09 16:36:12 +0000

    Dynamically generated schedule

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,62 +17,80 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-84551354-1', 'auto');
 ga('send', 'pageview');
 
-var talks = [];
-var $days = document.querySelectorAll("#schedule .day");
-$days.forEach(function($day, day){
-    var $items = $day.querySelectorAll(".item");
-    console.log("ITEMS:", $items);
-
-    $items.forEach(function($item){
-        const regex = /\([0-9]*/g;
-        const str = $item.querySelector("h3").innerText;
-        const duration = parseInt(regex.exec(str)[0].replace("(", ""));
-        var talk = {
-            "day": (day + 1),
-            "title": $item.querySelector(".talk-title").innerText,
-            "duration": duration,
-            "break": ($item.classList.contains("break") === true)
-        };
-        var $a = $item.querySelector("a");
-        if ($a !== null) {
-            talk.speaker = $item.querySelector("a").getAttribute("title");
-            talk.handle = $item.querySelector("a").getAttribute("href");
-        }
-        var $img = $item.querySelector("img");
-        if ($img !== null) {
-            talk.photo = $img.getAttribute("src");
-        }
-        var $description = $item.querySelector(".description");
-        if ($description !== null) {
-            talk.description = $description.innerText;
-        }
-        console.log("TALK:", talk);
-        talks.push(talk);
-    });
-});
+// var talks = [];
+// var $days = document.querySelectorAll("#schedule .day");
+// $days.forEach(function($day, day){
+//     var $items = $day.querySelectorAll(".item");
+//
+//     $items.forEach(function($item){
+//         const regex = /\([0-9]*/g;
+//         const str = $item.querySelector("h3").innerText;
+//         const duration = parseInt(regex.exec(str)[0].replace("(", ""));
+//         var talk = {
+//             "day": (day + 1),
+//             "title": $item.querySelector(".talk-title").innerText,
+//             "duration": duration,
+//             "break": ($item.classList.contains("break") === true)
+//         };
+//         var $a = $item.querySelector("a");
+//         if ($a !== null) {
+//             talk.speaker = $item.querySelector("a").getAttribute("title");
+//             talk.handle = $item.querySelector("a").getAttribute("href");
+//         }
+//         var $img = $item.querySelector("img");
+//         if ($img !== null) {
+//             talk.photo = $img.getAttribute("src");
+//         }
+//         var $description = $item.querySelector(".description");
+//         if ($description !== null) {
+//             talk.description = $description.innerText;
+//         }
+//         talks.push(talk);
+//     });
+// });
 
 
-console.log(JSON.stringify(talks));
+const GAP = 5;
+let day1 = moment("2017-02-16 10-0500");
+let day2 = moment("2017-02-17 10-0500");
+let $day1 = document.querySelector("#day1");
+let $day2 = document.querySelector("#day2");
+const tzString = String(String(new Date()).split("(")[1]).split(")")[0];
 
 var req = new XMLHttpRequest();
 req.onreadystatechange = function() {
     if (req.readyState == 4 && req.status == 200) {
         var response = JSON.parse(req.responseText);
         response.forEach(function(talk){
-            var $tpl = `
-                <div class="item ${(talk.break === true) ? 'break' : '' }">
-                    <div class="title">
-                        <div class="text">
-                            <h3>11:00AM EST (${talk.duration})</h3>
-                            <div class="talk-title">${talk.title}</div>
-                        </div>
-                        <a href="#eisaksen" title="Erik Isaksen" class="photo">
-                            <img src="${talk.photo}" />
-                        </a>
+
+            const time = (talk.day === 1) ? day1 : day2;
+            const timeLabel = time.format("LT");
+            let $item = document.createElement("div");
+            $item.classList.add("item");
+            if (talk.break === true) {
+                $item.classList.add("break");
+            }
+            $item.innerHTML = `               
+                <div class="title">
+                    <div class="text">
+                        <h3>${timeLabel} ${tzString} (${talk.duration} Min)</h3>
+                        <div class="talk-title">${talk.title}</div>
                     </div>
-                    <p class="description">Welcome! An introduction and overview of the following two days and important announcements.</p>
+                    <a href="${talk.handle}" title="${talk.speaker}" class="photo">
+                        <img src="${talk.photo}" />
+                    </a>
                 </div>
+                <p class="description">${talk.description}</p>                
             `;
+
+            if (talk.day === 1) {
+                $day1.appendChild($item);
+                day1 = moment(day1).add(talk.duration + GAP, 'm');
+            } else {
+                $day2.appendChild($item);
+                day2 = moment(day2).add(talk.duration + GAP, 'm');
+            }
+
         });
     }
 };
